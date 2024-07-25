c3f23b968c8aa8410a529a7c115d3b44beb6791b
DaWitChoi <dawit0310@gmail.com>
2024-07-25 10:28:17 +0900

    Title: 369 게임, Time: 151ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240725/369 게임/369-game.kt
@@ -0,0 +1,18 @@
+fun main(){
+    val n = readLine()?.trim()?.toInt()
+
+    val buffer = StringBuffer()
+
+    for(item in 1..n!!){
+        val str = item.toString()
+        if(str.contains("3") || str.contains("6") || str.contains("9")){
+            buffer.append(0)
+        } else{
+            buffer.append(item)
+        }
+        
+        buffer.append(" ")
+    }
+
+    println(buffer.toString().trim())
+}
